2a2e00a917b6f2701573be22e7554c6d85a10588
Tanuj Garg <tanujgarg@y7mail.com>
2019-12-11 16:19:23 +0530

    login component added

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = ''
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+    this.username = 'hello'
+    this.password = '12345679',
+      console.log(this.loginService.hello())
+
+  }
+
+  handleChange(event) {
+    if (event.target.name === 'username') {
+      if (!event.target.value.match(/@/)) {
+        alert('invalid email')
+      }
+    }
+    this[event.target.name] = event.target.value
+  }
+
+  submit() {
+    console.log(this.username, this.password)
   }
 
 }
